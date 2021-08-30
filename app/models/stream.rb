462a5b51d83eca0bd58590a2be1a9a4293786ce7
Brian Boehm <bri.boehm@gmail.com>
2021-08-29 22:58:49 -0400

    Added associations and validations

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,10 @@
 class Stream < ApplicationRecord
     has_one_attached :stream_graphic
+    belongs_to :user
+
+    validates :name, :description, :schedule, presence: true
+    validates :name, uniqueness: true
+
 
 
 end
