4d78d90336a024cc0e289908dc2b758142af699f
Brian Boehm <bri.boehm@gmail.com>
2021-09-02 09:37:10 -0400

    Added tables for user and streams.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_28_023954) do
+ActiveRecord::Schema.define(version: 2021_08_29_014125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2021_08_28_023954) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "streams", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "schedule"
+    t.datetime "schedule_dates"
+    t.datetime "schedule_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_streams_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +62,5 @@ ActiveRecord::Schema.define(version: 2021_08_28_023954) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "streams", "users"
 end
