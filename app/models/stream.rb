32f62aeeab6af7ee5e8242513ab5e8d74587da64
Brian Boehm <bri.boehm@gmail.com>
2021-09-11 16:03:15 -0400

    Added helper method to use Rails url_helper method to generate our image url

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -5,6 +5,10 @@ class Stream < ApplicationRecord
     validates :name, :description, :schedule, presence: true
     validates :name, uniqueness: true
 
+    def stream_graphic_url 
+        Rails.application.routes.url_helpers.url_for(stream_graphic) if stream_graphic.attached?
+    end
+
 
 
 end
